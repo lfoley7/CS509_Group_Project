8e61ef684dd5452f842f8a4e7ee59494b53cb806
lfoley7 <luke.c.foley@gmail.com>
2023-10-28 18:53:19 -0400

    Working Navigation

---
+++ the-cs/src/layout/Layout.js
@@ -0,0 +1,17 @@
+//import Login from "../login/Login";
+//import Navbar from '../navbar/Navbar';
+
+console.log("LAYOUT");
+
+const Layout = () => {
+    console.log("now we are here");
+    return (
+        <div style={{ marginTop: "7%" }}>
+            <p>In Layout!</p>
+            {/* <Navbar />
+            <Login /> */}
+        </div>
+    )
+};
+
+export default Layout;
