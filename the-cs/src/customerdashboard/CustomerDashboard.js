e2288df5c0849fc1da9b7f85f8f85251e16aafc6
MarekSGarbaczonek <msgarbaczonek@gmail.com>
2023-12-11 20:44:03 -0500

    Client New Multi Select Generate Inventory

--- the-cs/src/customerdashboard/CustomerDashboard.js
+++ the-cs/src/customerdashboard/CustomerDashboard.js
@@ -1,10 +1,15 @@
 import { useEffect } from "react";
+import * as React from 'react';
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import './CustomerDashboard.css';
-import $ from 'jquery';
-import { v4 as uuidv4 } from 'uuid';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import ListItemText from '@mui/material/ListItemText';
+import Select from '@mui/material/Select';
+import Checkbox from '@mui/material/Checkbox';
 
 const instance = axios.create({
     baseURL: 'https://iggshnplye.execute-api.us-east-2.amazonaws.com/Initial/'
@@ -16,34 +21,68 @@ function CustomerDashboard(props) {
     const PROCESSOR = ["Intel Xeon", "Intel i9", "Intel i7", "AMD Ryzen 9", "AMD Ryzen 7"];
     const GRAPHICS = ["NVIDIA GeForce RTX 4090", "NVIDIA GeForce RTX 4080", "AMD Radeon Pro W6300",
         "AMD Radeon Pro W6400", "Intel Integrated Graphics", "Intel UHD Graphics 730", "Intel UHD Graphics 770"];
-
     const navigate = useNavigate();
 
-    useEffect(() => {
-        // Getting the Company Names and Adding them to the Dropdown and Table
-        instance.post("fetchStore")
-            .then(function (response) {
-                for (let company of JSON.parse(response.data.body)) {
-                    let tableItem = document.createElement("td");
-                    tableItem.className = "generate-table-item"
-                    tableItem.innerHTML = company.STName;
-                    tableItem.setAttribute("data-UUID", company.StoreID)
-                    let tableGenerateButton = document.createElement("button");
-                    tableGenerateButton.innerHTML = "Generate Inventory"
-                    tableGenerateButton.onclick = () => { generateInventoryClick(company.StoreID) }
-                    let br = document.createElement("br");
-                    tableItem.appendChild(br);
-                    tableItem.appendChild(tableGenerateButton);
-                    document.getElementById("all-stores").appendChild(tableItem);
-                }
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    })
+    const fetchStores = async () => {
+        try {
+            const response = await instance.post("fetchStore");
+            return JSON.parse(response.data.body);
+        } catch (error) {
+            console.error("Error fetching stores:", error);
+            throw error;
+        }
+    };
+    
+    const MultipleSelectCheckmarks = ({ label }) => {
+        const [selectionName, setSelectionName] = React.useState([]);
+        const [storeNames, setStoreNames] = React.useState([]);
+    
+        const handleChange = (event) => {
+            const {
+                target: { value },
+            } = event;
+            setSelectionName( typeof value === 'string' ? value.split(',') : value, );
+            generateInventoryCustomerClick(value);
+        };
+    
+        const handleOpen = async () => {
+            try {
+                setStoreNames(await fetchStores());
+            } catch (error) {
+                console.error("Error in handleOpen:", error);
+            }
+        };
+    
+        return (
+            <div>
+                <FormControl sx={{ m: 1, width: 300 }}>
+                    <InputLabel id={`multiple-checkbox-label-${label}`}>{label}</InputLabel>
+                    <Select
+                        labelId={`multiple-checkbox-label-${label}`}
+                        id={`multiple-checkbox-${label}`}
+                        multiple
+                        value={selectionName}
+                        onChange={handleChange}
+                        onOpen={handleOpen}
+                        input={<OutlinedInput label={label} />}
+                        renderValue={(selected) => selected.join(', ')}
+                    >
+                        {storeNames.length > 0 ? (
+                            storeNames.map((name) => (
+                                <MenuItem key={name.STName} value={name.StoreID}>
+                                    <Checkbox checked={selectionName.indexOf(name.StoreID) > -1} />
+                                    <ListItemText primary={name.STName} />
+                                </MenuItem>
+                            ))
+                        ) : null}
+                    </Select>
+                </FormControl>
+            </div>
+        );
+    };
 
-    const generateInventoryClick = (StoreID) => {
-        instance.post("generateInventory", { "StoreID": StoreID })
+    const generateInventoryCustomerClick = (StoreIDs) => {
+        instance.post("GenerateInventoryCustomer", { "StoreIDs": StoreIDs })
             .then(function (response) {
                 let tr = document.getElementById("generate-inventory-table");
                 if (tr.childNodes.length > 0) {
@@ -53,27 +92,30 @@ function CustomerDashboard(props) {
                 }
                 for (let computer of JSON.parse(response.data.body)) {
                     const inventoryRow = document.createElement("tr");
+                    const storeName = document.createElement("td");
+                    storeName.innerHTML = computer.STName;
                     const name = document.createElement("td");
                     name.innerHTML = computer.CName;
                     const price = document.createElement("td");
-                    price.innerHTML = computer.CPrice;
+                    price.innerHTML = "$"+computer.CPrice;
                     const memory = document.createElement("td");
-                    memory.innerHTML = computer.CMemory;
+                    memory.innerHTML = computer.CMemory+"GB";
                     const storageSize = document.createElement("td");
-                    storageSize.innerHTML = computer.CStorageSize;
+                    storageSize.innerHTML = computer.CStorageSize+"GB";
                     const processor = document.createElement("td");
                     processor.innerHTML = PROCESSOR[+computer.CProcessor - 1];
                     const processorGen = document.createElement("td");
                     processorGen.innerHTML = PROCESSORGEN[+computer.CProcessorGen - 1];
                     const graphics = document.createElement("td");
                     graphics.innerHTML = GRAPHICS[+computer.CGraphics - 1];
+                    inventoryRow.appendChild(storeName);
                     inventoryRow.appendChild(name);
-                    inventoryRow.appendChild(price);
+                    inventoryRow.appendChild(graphics);
+                    inventoryRow.appendChild(processorGen);
+                    inventoryRow.appendChild(processor);
                     inventoryRow.appendChild(memory);
                     inventoryRow.appendChild(storageSize);
-                    inventoryRow.appendChild(processor);
-                    inventoryRow.appendChild(processorGen);
-                    inventoryRow.appendChild(graphics);
+                    inventoryRow.appendChild(price);
                     document.getElementById("generate-inventory-table").appendChild(inventoryRow);
                 }
             })
@@ -83,37 +125,34 @@ function CustomerDashboard(props) {
     }
 
     return (
-        <div className="sm-container">
-            <div>
-                <label>Want to become a Store Owner?&nbsp;</label>
-                <a onClick={() => { navigate("/register"); }}>Click here to register!</a>
+        <div className="Content-Container">
+            <div className="Filter-Container">
+                <div className="Filter-Item">
+                    <label>Want to become a Store Owner?&nbsp;</label>
+                    <a onClick={() => { navigate("/register"); }}>Click here to register!</a>
+                </div>
+                <div className="Filter-Item">
+                    <MultipleSelectCheckmarks label="Stores"/>
+                </div>
             </div>
-            <table>
-                <thead>
-                    <tr>
-                        Store Names:
-                    </tr>
-                </thead>
-                <tbody id="all-stores">
-                </tbody>
-            </table>
-            {/* <select id="inventory-store-options"></select>
-            <button onClick={generateInventoryClick}>{"Generate Selected Store Inventory"}</button> */}
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Price</th>
-                        <th>Memory</th>
-                        <th>Storage Size</th>
-                        <th>Processor</th>
-                        <th>Processor Gen</th>
-                        <th>Graphics</th>
-                    </tr>
-                </thead>
-                <tbody id="generate-inventory-table">
-                </tbody>
-            </table>
+            <div className="Table-Container">
+                <table className="Table-Custom">
+                    <thead>
+                        <tr>
+                            <th>Store</th>
+                            <th>Name</th>
+                            <th>Graphics</th>
+                            <th>Processor Gen</th>
+                            <th>Processor</th>
+                            <th>Memory</th>
+                            <th>Storage Size</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody id="generate-inventory-table">
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 };
